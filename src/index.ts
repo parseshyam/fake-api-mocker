3c46e14089398dea0c95e7e484dd7d474787baad
parse_shyam <parse.s@simformsolutions.com>
2021-09-21 17:32:39 +0530

    feat: Error Handling, Chunks, isBoolean

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 
-// import { schema, mockFakeAPI } from "./schema"
+// const { schema, mockFakeAPI } = require("./schema");
 export { schema, mockFakeAPI } from "./schema"
-// EXAMPLE 
+// EXAMPLE
+// const { schema, mockFakeAPI } = require("./schema");
 // const fakeSchema = {
 //   id_array: schema.isUUID().rows(3).done(),
 //   email_array: schema.isEmail({ isYopMail: true }).rows(1).done(),
@@ -9,10 +10,9 @@ export { schema, mockFakeAPI } from "./schema"
 //   name: schema.isObject({
 //     firstName: schema.isString().done(),
 //     lastName: schema.isString().done()
-//   }).rows(2).done()
+//   }).rows(2).done(),
+//   isPublished: schema.isBoolean().rows(10).done()
 // };
-
-
 // (async () => {
 //   console.log("MOCKING API CALL");
 //   const data = await mockFakeAPI(fakeSchema, 1000); // Schema and Resolve-Timeout (default 0);
